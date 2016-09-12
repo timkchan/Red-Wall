754490ff2824fe816d3c51418872d881fe6da335
Tim K. Chan <timchan@berkeley.edu>
2016-09-12 01:17:13 -0700

    added collectionViewLayout

--- RedWall/WallpaperCollectionViewController.swift
+++ RedWall/WallpaperCollectionViewController.swift
@@ -12,7 +12,25 @@ class WallpaperCollectionViewController: UICollectionViewController {
     
     //data source
     
+    private let leftAndRightPaddings: CGFloat = 8.0
+    private let numberOfItemsPerRow: CGFloat = 2.0
+    private let heightAdjustment: CGFloat = 30.0
     
+    
+    // MARK: - View controller life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let width = (collectionView!.frame.width - leftAndRightPaddings * (numberOfItemsPerRow + 1)) / numberOfItemsPerRow
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width - heightAdjustment)
+        
+        
+        
+    }
+    
+    
+    // MARK: - UICollectionView Data source
     // Number of sections.
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -20,7 +38,7 @@ class WallpaperCollectionViewController: UICollectionViewController {
     
     // Number of items in each section.
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5 // TK: to be modified.
+        return 30 // TK: to be modified.
     }
     
     private struct Storyboard {
