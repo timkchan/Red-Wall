a63524be8ed772f7fd4196294da729fe88f6c8a7
Tim K. Chan <timchan@berkeley.edu>
2016-09-12 02:52:45 -0700

    JSON Parsed

--- RedWall/WallpaperCollectionViewController.swift
+++ RedWall/WallpaperCollectionViewController.swift
@@ -14,13 +14,46 @@ class WallpaperCollectionViewController: UICollectionViewController {
     
     private let leftAndRightPaddings: CGFloat = 8.0
     private let numberOfItemsPerRow: CGFloat = 2.0
-    private let heightAdjustment: CGFloat = 30.0
+    private let heightAdjustment: CGFloat = 50.0
     
     
     // MARK: - View controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Fetch Reddit JSON content
+        let sourceURL = URL(string: "https://www.reddit.com/r/wallpapers/.json")
+        let task = URLSession.shared.dataTask(with: sourceURL!) { (data, response, error) in
+            if error != nil {
+                print(error)
+            } else {
+                if let URLConent = data {
+                    do {
+                        let jsonResult = try JSONSerialization.jsonObject(with: URLConent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+
+                        if let jsonData = jsonResult["data"] as? NSDictionary {
+                            if let items = jsonData["children"] as? NSArray {
+                                for item in items {
+                                    let itemD:NSDictionary = (item as? NSDictionary)!
+
+                                        print("------------------------------------")
+                                        print(itemD["data"] as! NSDictionary)
+                                        
+
+                                }
+                            }
+                        }
+                    } catch {
+                        print("JSON processing failed")
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+        
+        
+        // Setup the layout of collection view
         let width = (collectionView!.frame.width - leftAndRightPaddings * (numberOfItemsPerRow + 1)) / numberOfItemsPerRow
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width - heightAdjustment)
