64302ab5b0e0d5bcf802d00817021c31cfa63f44
Tim K. Chan <timchan@berkeley.edu>
2016-09-12 00:56:22 -0700

    collection view set up

---
+++ RedWall/WallpaperCollectionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  wallpaperCollectionViewController.swift
+//  RedWall
+//
+//  Created by Tim Kit Chan on 12/09/16.
+//  Copyright © 2016 AppBee. All rights reserved.
+//
+
+import UIKit
+
+class WallpaperCollectionViewController: UICollectionViewController {
+    
+    //data source
+    
+    
+    // Number of sections.
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    // Number of items in each section.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5 // TK: to be modified.
+    }
+    
+    private struct Storyboard {
+        static let CellIdentifier = "WallpaperCell"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.CellIdentifier, for: indexPath) as UICollectionViewCell
+        
+        return cell
+    }
+    
+}
